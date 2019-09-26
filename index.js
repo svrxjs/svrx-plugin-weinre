cad9d7f5f5afc67a8d29cadd19094f092c306fd2
Cody Chan <int64ago@gmail.com>
2019-09-26 16:00:30 +0800

    fix: fix lint

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const ffp = require('find-free-port');
+const os = require('os');
 const weinre = require('./weinre/lib/weinre');
 const utils = require('./weinre/lib/utils');
-const os = require('os');
 
 function getExternalIp() {
   const ifaces = os.networkInterfaces();
@@ -20,31 +20,31 @@ function getExternalIp() {
 module.exports = {
   configSchema: {
     httpPort: {
-      type: 'number'
+      type: 'number',
     },
     boundHost: {
       type: 'string',
-      default: getExternalIp()
+      default: getExternalIp(),
     },
     verbose: {
       type: 'boolean',
-      default: false
+      default: false,
     },
     debug: {
       type: 'boolean',
-      default: false
+      default: false,
     },
     readTimeout: {
       type: 'number',
-      default: 5
+      default: 5,
     },
     deathTimeout: {
       type: 'number',
-      default: 15
-    }
+      default: 15,
+    },
   },
   assets: {
-    script: config => {
+    script: (config) => {
       const boundHost = config.get('boundHost');
       const httpPort = config.get('httpPort');
       return `
@@ -54,21 +54,20 @@ module.exports = {
           document.body.appendChild(script);
         })();
       `;
-    }
+    },
   },
   hooks: {
     async onCreate({ config, logger, events }) {
-
       utils.log = (message) => {
         logger.debug(message);
-      }
+      };
 
       utils.notify = (message) => {
         logger.notify(message);
-      }
+      };
 
       events.on('ready', async () => {
-        const [ vaildPort ] = await ffp(config.get('$.port'));
+        const [vaildPort] = await ffp(config.get('$.port'));
         const httpPort = config.get('httpPort') || vaildPort;
         config.set('httpPort', httpPort);
 
@@ -78,11 +77,11 @@ module.exports = {
           verbose: config.get('verbose'),
           debug: config.get('debug'),
           readTimeout: config.get('readTimeout'),
-          deathTimeout: config.get('deathTimeout')
+          deathTimeout: config.get('deathTimeout'),
         };
 
         weinre.run(opts);
       });
-    }
-  }
+    },
+  },
 };
